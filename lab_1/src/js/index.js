26c6a8a364e36c1f53ec455e84152b4a846682eb
Eddie Nubes <53977359+eddienubes@users.noreply.github.com>
2020-09-06 19:51:52 +0300

    First Day Changes
    - Nav bar edits
    - Carousel creation
    - Cards with animation

--- lab_1/src/js/index.js
+++ lab_1/src/js/index.js
@@ -5,4 +5,8 @@ import 'bootstrap';
 import "../css/styles.css";
 import "../scss/style.scss";
 import "../assets/img/white_logo.png";
+import "../assets/img/koteika.jpg";
+import "../assets/img/leha.jpg";
+import "../assets/img/mikfat.jpg";
+
 
