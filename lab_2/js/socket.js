f98aa3db1ffe7a1852922a2fdca2a4ea7277635b
andreybashtovoy <39826086+andreybashtovoy@users.noreply.github.com>
2020-09-13 18:59:19 +0300

    fixed html again

--- lab_2/js/socket.js
+++ lab_2/js/socket.js
@@ -1,4 +1,4 @@
-var socket = io.connect();
+const socket = io.connect('localhost:9000');
 
 // $form.submit(function(event) {
 //     event.preventDefault();
@@ -9,3 +9,16 @@ var socket = io.connect();
 socket.on('action', function(data) {
     $all_messages.append("<div class='alert alert-" + data.className + "'><b>" + data.name + "</b>: " + data.mess + "</div>");
 });
+
+$('.send-message-form').onsubmit(() => {
+    apppend_message($('.send-message-textarea').value());
+});
+
+function apppend_message(text){
+    $('.chat').append('<section class="message">\n' +
+        '                <img width="50" height="50"\n' +
+        '                     src="https://www.meme-arsenal.com/memes/547f571a0ae09614a2e9d93260e15f70.jpg" alt="avatar">\n' +
+        '                <div class="content"><h3 style="color: yellow">Username</h3>\n' +
+        '                    <p>'+text+'</p></div>\n' +
+        '            </section>');
+}
