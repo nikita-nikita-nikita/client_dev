f7be93033ecaa646fc727fd1bd3d1a9f92027543
Nikita <neymarenko123@gmail.com>
2020-09-13 16:01:48 +0300

    Create header

---
+++ lab_2/index.js
@@ -0,0 +1,6 @@
+const avatars = [
+    "https://pic.rutube.ru/video/f1/6a/f16abc14d8206874b770e004d9dcc6da.jpg?size=m",
+    "https://bipbap.ru/wp-content/uploads/2016/11/80835132IMGI1506m.jpg",
+    "https://www.meme-arsenal.com/memes/547f571a0ae09614a2e9d93260e15f70.jpg",
+    "https://bipbap.ru/wp-content/uploads/2016/11/1455682516_001.jpg"
+];
--- lab_2/server.js
+++ lab_2/server.js
@@ -3,4 +3,6 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
+
+
 server.listen(9000);
