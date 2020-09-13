7bd39143a9f4043bf7249ea5fc22d2d659111e1d
Nikita <neymarenko123@gmail.com>
2020-09-13 18:25:04 +0300

    Finally did it
    All view stuff did

--- lab_2/js/index.js
+++ lab_2/js/index.js
@@ -4,3 +4,4 @@ const avatars = [
     "https://www.meme-arsenal.com/memes/547f571a0ae09614a2e9d93260e15f70.jpg",
     "https://bipbap.ru/wp-content/uploads/2016/11/1455682516_001.jpg"
 ];
+// todo add normal scroll
