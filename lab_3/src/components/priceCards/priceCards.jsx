0de224f3c0b7d0327ae618704b2e5eeedeb2cc9a
Eddie Nubes <53977359+eddienubes@users.noreply.github.com>
2020-10-17 16:52:42 +0300

    Mainly cards
    - Added normal slider animation
    - Fixed card scale bug
    - added 2 more mp3s

--- lab_3/src/components/priceCards/priceCards.jsx
+++ lab_3/src/components/priceCards/priceCards.jsx
@@ -1,19 +1,40 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Card, Image, Button} from 'semantic-ui-react';
 import "./priceCards.scss";
 import Accordion from 'react-bootstrap/Accordion'
-import {faTag} from "@fortawesome/free-solid-svg-icons";
+import {faAngleUp, faTag, faAngleDown} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Switch from "react-bootstrap/Switch";
-import Container from 'react-bootstrap/Container'
+import {AccordionContext, useAccordionToggle} from "react-bootstrap";
+
+const ContextAwareToggle = ({eventKey, callback }) => {
+    const currentEventKey = useContext(AccordionContext);
+
+    const decoratedOnClick = useAccordionToggle(
+        eventKey,
+        () => callback && callback(eventKey),
+    );
+
+    const isCurrentEventKey = currentEventKey === eventKey;
+
+    let buttonContent = isCurrentEventKey ?
+        <span><FontAwesomeIcon icon={faAngleUp}/> LESS</span> : <span><FontAwesomeIcon icon={faAngleDown}/> MORE</span>;
+    return (
+        <button
+            type="button"
+            onClick={decoratedOnClick}
+            className="card-footer-button"
+        >
+            {buttonContent}
+        </button>
+    );
+}
 
 const PriceCards = () => {
-    const [isActive, setActive] = useState(false);
-
-    const buttonNameHandler = () => isActive ? "LESS" : "MORE";
+    const [activeKey, setActiveKey] = useState(null);
 
     return (
-        <Switch className="grid">
+        <div className="card-container">
             <Card className="card">
                 <div className="head">
                     <p>POPULAR</p>
@@ -38,10 +59,10 @@ const PriceCards = () => {
                 </div>
                 <div className="card-footer">
                     <div className="card-footer-caption">
-                        <span><FontAwesomeIcon className="icon" icon={faTag}/> BULK DEALS</span>
+                        <span><FontAwesomeIcon className="icon" icon={faTag}/> FREE BEATS</span>
                         <p>BUY 2, GET 1 FREE</p>
                     </div>
-                    <Accordion>
+                    <Accordion activeKey={activeKey} onSelect={e => setActiveKey(e)}>
                         <Accordion.Collapse className="card-footer-content" eventKey="0">
                             <div>
                                 <p>
@@ -62,10 +83,7 @@ const PriceCards = () => {
                             </div>
                         </Accordion.Collapse>
                         <div className="button-container">
-                            <Accordion.Toggle className="card-footer-button" as={Button} variant="link" eventKey="0"
-                                              onClick={() => setActive(!isActive)}>
-                                <span>{buttonNameHandler()}</span>
-                            </Accordion.Toggle>
+                            <ContextAwareToggle eventKey="0"/>
                         </div>
 
                     </Accordion>
@@ -100,8 +118,8 @@ const PriceCards = () => {
                         <p>BUY 2, GET 1 FREE</p>
                     </div>
 
-                    <Accordion>
-                        <Accordion.Collapse className="card-footer-content" eventKey="0">
+                    <Accordion activeKey={activeKey} onSelect={e => setActiveKey(e)}>
+                        <Accordion.Collapse className="card-footer-content" eventKey="1">
                             <div>
                                 <p>
                                     BUY 3, GET 2 FREE
@@ -121,10 +139,7 @@ const PriceCards = () => {
                             </div>
                         </Accordion.Collapse>
                         <div className="button-container">
-                            <Accordion.Toggle className="card-footer-button" as={Button} variant="link" eventKey="0"
-                                              onClick={() => setActive(!isActive)}>
-                                <span>{buttonNameHandler()}</span>
-                            </Accordion.Toggle>
+                            <ContextAwareToggle eventKey="1"/>
                         </div>
 
                     </Accordion>
@@ -158,8 +173,8 @@ const PriceCards = () => {
                     <p>BUY 2, GET 1 FREE</p>
                 </div>
 
-                <Accordion>
-                    <Accordion.Collapse className="card-footer-content" eventKey="0">
+                <Accordion activeKey={activeKey} onSelect={e => setActiveKey(e)}>
+                    <Accordion.Collapse className="card-footer-content" eventKey="2">
                         <div>
                             <p>
                                 BUY 3, GET 2 FREE
@@ -179,133 +194,125 @@ const PriceCards = () => {
                         </div>
                     </Accordion.Collapse>
                     <div className="button-container">
-                        <Accordion.Toggle className="card-footer-button" as={Button} variant="link" eventKey="0"
-                                          onClick={() => setActive(!isActive)}>
-                            <span>{buttonNameHandler()}</span>
-                        </Accordion.Toggle>
-                    </div>
-
-                </Accordion>
-            </div>
-
-        </Card><Card className="card">
-            <div className="head">
-                <p>POPULAR</p>
-                <div className="license-type">
-                    Unlimited Lease
-                </div>
-                <div className="price">
-                    $219.99
-                </div>
-                <div className="per-unit">
-                    PER UNIT
-                </div>
-            </div>
-            <div className="description">
-                <ul>
-                    <li>Untagged Wav Track stems</li>
-                    <li>Unlimited Distribution</li>
-                    <li>Unlimited Commercial Use</li>
-                    <li>Able to put song on all platforms (Spotify, Apple Music etc.)</li>
-                    <li>Must Credit (prod. eddienubes)</li>
-                </ul>
-            </div>
-            <div className="card-footer">
-                <div className="card-footer-caption">
-                    <span><FontAwesomeIcon className="icon" icon={faTag}/> BULK DEALS</span>
-                    <p>BUY 2, GET 1 FREE</p>
-                </div>
-
-                <Accordion>
-                    <Accordion.Collapse className="card-footer-content" eventKey="0">
-                        <div>
-                            <p>
-                                BUY 3, GET 2 FREE
-                            </p>
-                            <p>
-                                BUY 4, GET 3 FREE
-                            </p>
-                            <p>
-                                BUY 5, GET 4 FREE
-                            </p>
-                            <p>
-                                BUY 6, GET 5 FREE
-                            </p>
-                            <p>
-                                BUY 10, GET 10 FREE
-                            </p>
-                        </div>
-                    </Accordion.Collapse>
-                    <div className="button-container">
-                        <Accordion.Toggle className="card-footer-button" as={Button} variant="link" eventKey="0"
-                                          onClick={() => setActive(!isActive)}>
-                            <span>{buttonNameHandler()}</span>
-                        </Accordion.Toggle>
-                    </div>
-
-                </Accordion>
-            </div>
-
-        </Card><Card className="card">
-            <div className="head">
-                <p>POPULAR</p>
-                <div className="license-type">
-                    Unlimited Lease
-                </div>
-                <div className="price">
-                    $219.99
-                </div>
-                <div className="per-unit">
-                    PER UNIT
-                </div>
-            </div>
-            <div className="description">
-                <ul>
-                    <li>Untagged Wav Track stems</li>
-                    <li>Unlimited Distribution</li>
-                    <li>Unlimited Commercial Use</li>
-                    <li>Able to put song on all platforms (Spotify, Apple Music etc.)</li>
-                    <li>Must Credit (prod. eddienubes)</li>
-                </ul>
-            </div>
-            <div className="card-footer">
-                <div className="card-footer-caption">
-                    <span><FontAwesomeIcon className="icon" icon={faTag}/> BULK DEALS</span>
-                    <p>BUY 2, GET 1 FREE</p>
-                </div>
-
-                <Accordion>
-                    <Accordion.Collapse className="card-footer-content" eventKey="0">
-                        <div>
-                            <p>
-                                BUY 3, GET 2 FREE
-                            </p>
-                            <p>
-                                BUY 4, GET 3 FREE
-                            </p>
-                            <p>
-                                BUY 5, GET 4 FREE
-                            </p>
-                            <p>
-                                BUY 6, GET 5 FREE
-                            </p>
-                            <p>
-                                BUY 10, GET 10 FREE
-                            </p>
-                        </div>
-                    </Accordion.Collapse>
-                    <div className="button-container">
-                        <Accordion.Toggle className="card-footer-button" as={Button} variant="link" eventKey="0"
-                                          onClick={() => setActive(!isActive)}>
-                            <span>{buttonNameHandler()}</span>
-                        </Accordion.Toggle>
+                        <ContextAwareToggle eventKey="2"/>
                     </div>
 
                 </Accordion>
             </div>
 
         </Card>
-        </Switch>
+            <Card className="card">
+            <div className="head">
+                <p>POPULAR</p>
+                <div className="license-type">
+                    Unlimited Lease
+                </div>
+                <div className="price">
+                    $219.99
+                </div>
+                <div className="per-unit">
+                    PER UNIT
+                </div>
+            </div>
+            <div className="description">
+                <ul>
+                    <li>Untagged Wav Track stems</li>
+                    <li>Unlimited Distribution</li>
+                    <li>Unlimited Commercial Use</li>
+                    <li>Able to put song on all platforms (Spotify, Apple Music etc.)</li>
+                    <li>Must Credit (prod. eddienubes)</li>
+                </ul>
+            </div>
+            <div className="card-footer">
+                <div className="card-footer-caption">
+                    <span><FontAwesomeIcon className="icon" icon={faTag}/> BULK DEALS</span>
+                    <p>BUY 2, GET 1 FREE</p>
+                </div>
+
+                <Accordion activeKey={activeKey} onSelect={e => setActiveKey(e)}>
+                    <Accordion.Collapse className="card-footer-content" eventKey="3">
+                        <div>
+                            <p>
+                                BUY 3, GET 2 FREE
+                            </p>
+                            <p>
+                                BUY 4, GET 3 FREE
+                            </p>
+                            <p>
+                                BUY 5, GET 4 FREE
+                            </p>
+                            <p>
+                                BUY 6, GET 5 FREE
+                            </p>
+                            <p>
+                                BUY 10, GET 10 FREE
+                            </p>
+                        </div>
+                    </Accordion.Collapse>
+                    <div className="button-container">
+                        <ContextAwareToggle eventKey="3"/>
+                    </div>
+
+                </Accordion>
+            </div>
+
+        </Card><Card className="card">
+            <div className="head">
+                <p>POPULAR</p>
+                <div className="license-type">
+                    Unlimited Lease
+                </div>
+                <div className="price">
+                    $219.99
+                </div>
+                <div className="per-unit">
+                    PER UNIT
+                </div>
+            </div>
+            <div className="description">
+                <ul>
+                    <li>Untagged Wav Track stems</li>
+                    <li>Unlimited Distribution</li>
+                    <li>Unlimited Commercial Use</li>
+                    <li>Able to put song on all platforms (Spotify, Apple Music etc.)</li>
+                    <li>Must Credit (prod. eddienubes)</li>
+                </ul>
+            </div>
+            <div className="card-footer">
+                <div className="card-footer-caption">
+                    <span><FontAwesomeIcon className="icon" icon={faTag}/> BULK DEALS</span>
+                    <p>BUY 2, GET 1 FREE</p>
+                </div>
+
+                <Accordion activeKey={activeKey} onSelect={e => setActiveKey(e)}>
+                    <Accordion.Collapse className="card-footer-content" eventKey="4">
+                        <div>
+                            <p>
+                                BUY 3, GET 2 FREE
+                            </p>
+                            <p>
+                                BUY 4, GET 3 FREE
+                            </p>
+                            <p>
+                                BUY 5, GET 4 FREE
+                            </p>
+                            <p>
+                                BUY 6, GET 5 FREE
+                            </p>
+                            <p>
+                                BUY 10, GET 10 FREE
+                            </p>
+                        </div>
+                    </Accordion.Collapse>
+                    <div className="button-container">
+                        <ContextAwareToggle eventKey="4"/>
+                    </div>
+
+                </Accordion>
+            </div>
+
+        </Card>
+        </div>
 
     )
 };
