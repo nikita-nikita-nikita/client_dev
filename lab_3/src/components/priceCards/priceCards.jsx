68fcd42f3889e0bf9366b322fe7bca0498b1f8be
Eddie Nubes <53977359+eddienubes@users.noreply.github.com>
2020-10-17 04:56:54 +0300

    added cards with slide animation
    ya zaebals9

--- lab_3/src/components/priceCards/priceCards.jsx
+++ lab_3/src/components/priceCards/priceCards.jsx
@@ -1,33 +1,313 @@
-import React from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import React, {useState} from 'react';
+import {Card, Image, Button} from 'semantic-ui-react';
 import "./priceCards.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import Accordion from 'react-bootstrap/Accordion'
+import {faTag} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Switch from "react-bootstrap/Switch";
+import Container from 'react-bootstrap/Container'
 
-const PriceCards = () => (
-    <Card className="card">
-        <div className="head">
-            <p>POPULAR</p>
-            <div className="license-type">
-                Unlimited Lease
-            </div>
-            <div className="price">
-                $219.99
-            </div>
-            <div className="per-unit">
-                PER UNIT
-            </div>
-        </div>
-        <div className="description">
+const PriceCards = () => {
+    const [isActive, setActive] = useState(false);
 
-            <p><FontAwesomeIcon icon={faCircle}/>ыфвывфывUntagged Wav Track stems</p>
-            <p><FontAwesomeIcon icon={faCircle}/>Unlimited Distribution</p>
-            <p><FontAwesomeIcon icon={faCircle}/>Unlimited Commercial Use</p>
-            <p><FontAwesomeIcon icon={faCircle}/>Able to put song on all platforms (Spotify, Apple Music etc.)</p>
-            <p><FontAwesomeIcon icon={faCircle}/>Must Credit (prod. eddienubes)</p>
+    const buttonNameHandler = () => isActive ? "LESS" : "MORE";
 
-        </div>
-    </Card>
-);
+    return (
+        <Switch className="grid">
+            <Card className="card">
+                <div className="head">
+                    <p>POPULAR</p>
+                    <div className="license-type">
+                        Unlimited Lease
+                    </div>
+                    <div className="price">
+                        $219.99
+                    </div>
+                    <div className="per-unit">
+                        PER UNIT
+                    </div>
+                </div>
+                <div className="description">
+                    <ul>
+                        <li>Untagged Wav Track stems</li>
+                        <li>Unlimited Distribution</li>
+                        <li>Unlimited Commercial Use</li>
+                        <li>Able to put song on all platforms (Spotify, Apple Music etc.)</li>
+                        <li>Must Credit (prod. eddienubes)</li>
+                    </ul>
+                </div>
+                <div className="card-footer">
+                    <div className="card-footer-caption">
+                        <span><FontAwesomeIcon className="icon" icon={faTag}/> BULK DEALS</span>
+                        <p>BUY 2, GET 1 FREE</p>
+                    </div>
+                    <Accordion>
+                        <Accordion.Collapse className="card-footer-content" eventKey="0">
+                            <div>
+                                <p>
+                                    BUY 3, GET 2 FREE
+                                </p>
+                                <p>
+                                    BUY 4, GET 3 FREE
+                                </p>
+                                <p>
+                                    BUY 5, GET 4 FREE
+                                </p>
+                                <p>
+                                    BUY 6, GET 5 FREE
+                                </p>
+                                <p>
+                                    BUY 10, GET 10 FREE
+                                </p>
+                            </div>
+                        </Accordion.Collapse>
+                        <div className="button-container">
+                            <Accordion.Toggle className="card-footer-button" as={Button} variant="link" eventKey="0"
+                                              onClick={() => setActive(!isActive)}>
+                                <span>{buttonNameHandler()}</span>
+                            </Accordion.Toggle>
+                        </div>
+
+                    </Accordion>
+                </div>
+
+            </Card>
+            <Card className="card">
+                <div className="head">
+                    <p>POPULAR</p>
+                    <div className="license-type">
+                        Unlimited Lease
+                    </div>
+                    <div className="price">
+                        $219.99
+                    </div>
+                    <div className="per-unit">
+                        PER UNIT
+                    </div>
+                </div>
+                <div className="description">
+                    <ul>
+                        <li>Untagged Wav Track stems</li>
+                        <li>Unlimited Distribution</li>
+                        <li>Unlimited Commercial Use</li>
+                        <li>Able to put song on all platforms (Spotify, Apple Music etc.)</li>
+                        <li>Must Credit (prod. eddienubes)</li>
+                    </ul>
+                </div>
+                <div className="card-footer">
+                    <div className="card-footer-caption">
+                        <span><FontAwesomeIcon className="icon" icon={faTag}/> BULK DEALS</span>
+                        <p>BUY 2, GET 1 FREE</p>
+                    </div>
+
+                    <Accordion>
+                        <Accordion.Collapse className="card-footer-content" eventKey="0">
+                            <div>
+                                <p>
+                                    BUY 3, GET 2 FREE
+                                </p>
+                                <p>
+                                    BUY 4, GET 3 FREE
+                                </p>
+                                <p>
+                                    BUY 5, GET 4 FREE
+                                </p>
+                                <p>
+                                    BUY 6, GET 5 FREE
+                                </p>
+                                <p>
+                                    BUY 10, GET 10 FREE
+                                </p>
+                            </div>
+                        </Accordion.Collapse>
+                        <div className="button-container">
+                            <Accordion.Toggle className="card-footer-button" as={Button} variant="link" eventKey="0"
+                                              onClick={() => setActive(!isActive)}>
+                                <span>{buttonNameHandler()}</span>
+                            </Accordion.Toggle>
+                        </div>
+
+                    </Accordion>
+                </div>
+
+            </Card><Card className="card">
+            <div className="head">
+                <p>POPULAR</p>
+                <div className="license-type">
+                    Unlimited Lease
+                </div>
+                <div className="price">
+                    $219.99
+                </div>
+                <div className="per-unit">
+                    PER UNIT
+                </div>
+            </div>
+            <div className="description">
+                <ul>
+                    <li>Untagged Wav Track stems</li>
+                    <li>Unlimited Distribution</li>
+                    <li>Unlimited Commercial Use</li>
+                    <li>Able to put song on all platforms (Spotify, Apple Music etc.)</li>
+                    <li>Must Credit (prod. eddienubes)</li>
+                </ul>
+            </div>
+            <div className="card-footer">
+                <div className="card-footer-caption">
+                    <span><FontAwesomeIcon className="icon" icon={faTag}/> BULK DEALS</span>
+                    <p>BUY 2, GET 1 FREE</p>
+                </div>
+
+                <Accordion>
+                    <Accordion.Collapse className="card-footer-content" eventKey="0">
+                        <div>
+                            <p>
+                                BUY 3, GET 2 FREE
+                            </p>
+                            <p>
+                                BUY 4, GET 3 FREE
+                            </p>
+                            <p>
+                                BUY 5, GET 4 FREE
+                            </p>
+                            <p>
+                                BUY 6, GET 5 FREE
+                            </p>
+                            <p>
+                                BUY 10, GET 10 FREE
+                            </p>
+                        </div>
+                    </Accordion.Collapse>
+                    <div className="button-container">
+                        <Accordion.Toggle className="card-footer-button" as={Button} variant="link" eventKey="0"
+                                          onClick={() => setActive(!isActive)}>
+                            <span>{buttonNameHandler()}</span>
+                        </Accordion.Toggle>
+                    </div>
+
+                </Accordion>
+            </div>
+
+        </Card><Card className="card">
+            <div className="head">
+                <p>POPULAR</p>
+                <div className="license-type">
+                    Unlimited Lease
+                </div>
+                <div className="price">
+                    $219.99
+                </div>
+                <div className="per-unit">
+                    PER UNIT
+                </div>
+            </div>
+            <div className="description">
+                <ul>
+                    <li>Untagged Wav Track stems</li>
+                    <li>Unlimited Distribution</li>
+                    <li>Unlimited Commercial Use</li>
+                    <li>Able to put song on all platforms (Spotify, Apple Music etc.)</li>
+                    <li>Must Credit (prod. eddienubes)</li>
+                </ul>
+            </div>
+            <div className="card-footer">
+                <div className="card-footer-caption">
+                    <span><FontAwesomeIcon className="icon" icon={faTag}/> BULK DEALS</span>
+                    <p>BUY 2, GET 1 FREE</p>
+                </div>
+
+                <Accordion>
+                    <Accordion.Collapse className="card-footer-content" eventKey="0">
+                        <div>
+                            <p>
+                                BUY 3, GET 2 FREE
+                            </p>
+                            <p>
+                                BUY 4, GET 3 FREE
+                            </p>
+                            <p>
+                                BUY 5, GET 4 FREE
+                            </p>
+                            <p>
+                                BUY 6, GET 5 FREE
+                            </p>
+                            <p>
+                                BUY 10, GET 10 FREE
+                            </p>
+                        </div>
+                    </Accordion.Collapse>
+                    <div className="button-container">
+                        <Accordion.Toggle className="card-footer-button" as={Button} variant="link" eventKey="0"
+                                          onClick={() => setActive(!isActive)}>
+                            <span>{buttonNameHandler()}</span>
+                        </Accordion.Toggle>
+                    </div>
+
+                </Accordion>
+            </div>
+
+        </Card><Card className="card">
+            <div className="head">
+                <p>POPULAR</p>
+                <div className="license-type">
+                    Unlimited Lease
+                </div>
+                <div className="price">
+                    $219.99
+                </div>
+                <div className="per-unit">
+                    PER UNIT
+                </div>
+            </div>
+            <div className="description">
+                <ul>
+                    <li>Untagged Wav Track stems</li>
+                    <li>Unlimited Distribution</li>
+                    <li>Unlimited Commercial Use</li>
+                    <li>Able to put song on all platforms (Spotify, Apple Music etc.)</li>
+                    <li>Must Credit (prod. eddienubes)</li>
+                </ul>
+            </div>
+            <div className="card-footer">
+                <div className="card-footer-caption">
+                    <span><FontAwesomeIcon className="icon" icon={faTag}/> BULK DEALS</span>
+                    <p>BUY 2, GET 1 FREE</p>
+                </div>
+
+                <Accordion>
+                    <Accordion.Collapse className="card-footer-content" eventKey="0">
+                        <div>
+                            <p>
+                                BUY 3, GET 2 FREE
+                            </p>
+                            <p>
+                                BUY 4, GET 3 FREE
+                            </p>
+                            <p>
+                                BUY 5, GET 4 FREE
+                            </p>
+                            <p>
+                                BUY 6, GET 5 FREE
+                            </p>
+                            <p>
+                                BUY 10, GET 10 FREE
+                            </p>
+                        </div>
+                    </Accordion.Collapse>
+                    <div className="button-container">
+                        <Accordion.Toggle className="card-footer-button" as={Button} variant="link" eventKey="0"
+                                          onClick={() => setActive(!isActive)}>
+                            <span>{buttonNameHandler()}</span>
+                        </Accordion.Toggle>
+                    </div>
+
+                </Accordion>
+            </div>
+
+        </Card>
+        </Switch>
+
+    )
+};
 
 export default PriceCards;
