a939584610f3432aca4a7a8495a518df1b15adf8
Nikita <neymarenko123@gmail.com>
2020-12-09 15:40:12 +0200

    add default middlewares

--- lab_4/server.js
+++ lab_4/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 const port = env.app.port||8080;
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen( port, () => {
     console.log( `server started at port ${ port }`);
