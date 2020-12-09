0957fc4821b0e82e8a4e1b4862793bad1e88ae8f
andreybashtovoy <39826086+andreybashtovoy@users.noreply.github.com>
2020-12-09 20:54:07 +0200

    start of db

--- lab_4/server.js
+++ lab_4/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const {DB} = require('./database/database')
 
 const app = express();
-const port = env.app.port||8080;
+const port = 5000;
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+DB.request(`SELECT * FROM users`);
+
 
 app.listen( port, () => {
     console.log( `server started at port ${ port }`);
