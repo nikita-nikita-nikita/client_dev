4690e62a42fe6f9757afeb969858b6fde08a2bad
Eddie Nubes <53977359+eddienubes@users.noreply.github.com>
2020-12-16 15:16:41 +0200

    Update BasketRow.jsx

--- lab_3/src/components/BasketRow/BasketRow.jsx
+++ lab_3/src/components/BasketRow/BasketRow.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './styleBasketRow.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import {faTimes, faPlay} from "@fortawesome/free-solid-svg-icons";
 import {LicenseBox} from "../priceCards";
 import {Table} from "semantic-ui-react";
 import {useDispatch} from "react-redux";
@@ -12,9 +12,10 @@ const BasketRow = ({id, product, amount, licenseType, imgUrl}) => {
     return (
         <Table.Row className="main-row">
             <Table.Cell verticalAlign="middle" textAlign="center" className="img-container row-item" sm={2}>
-                <img className='img-cart'
-                     src={imgUrl}
-                     alt="Track"/>
+                    <FontAwesomeIcon className='play-button-cart' icon={faPlay}/>
+                    <img className='img-cart'
+                        src={imgUrl}
+                        alt="Track"/>
             </Table.Cell>
             <Table.Cell textAlign="left" verticalAlign="middle" className="row-item">
                 {product}
@@ -28,9 +29,9 @@ const BasketRow = ({id, product, amount, licenseType, imgUrl}) => {
             <Table.Cell textAlign="center" width={1} verticalAlign="middle" className="row-item button-license">
                 <LicenseBox className="button" text="License"/>
             </Table.Cell>
-            <Table.Cell width={1} verticalAlign="middle" className="row-item">
-                <button className="remove-from-cart-button" onClick={() => {dispatch(removedFromCart(id))}}><
-                    FontAwesomeIcon className='basket__remove-item' icon={faTimes}/>
+            <Table.Cell width={1} verticalAlign="middle" textAlign="center" className="row-item remove-from-cart-button-container">
+                <button className="remove-from-cart-button" onClick={() => {dispatch(removedFromCart(id))}}>
+                    <FontAwesomeIcon className='basket__remove-item' icon={faTimes}/>
                 </button>
             </Table.Cell>
         </Table.Row>
